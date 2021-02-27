e18a1aed80e2560980ab15f42e86f1ec7cd7a82e
Philipp Regling <philippregling@googlemail.com>
2021-02-27 16:25:51 +0100

    Expanding

--- app/src/main/java/com/example/composepegel/ui/station/Station.kt
+++ app/src/main/java/com/example/composepegel/ui/station/Station.kt
@@ -11,11 +11,19 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,9 +41,7 @@ import dev.chrisbanes.accompanist.glide.GlideImage
 fun Station(
     navController: NavController,
     stationUuid: String,
-    viewModel: StationViewModel = viewModel(
-        factory = stationViewModelFactory(stationUuid)
-    )
+    viewModel: StationViewModel = viewModel()
 ) {
 
     val viewState by viewModel.state.observeAsState(StationState.InProgress)
@@ -66,46 +72,69 @@ fun StationDetails(station: StationModel) {
     }
 }
 
+class StationInfoCardState(expanded: Boolean) {
+    var expanded: Boolean by mutableStateOf(expanded)
+}
+
 @Composable
-fun StationInfo(station: StationModel) {
+fun StationInfo(
+    station: StationModel,
+    expandState: StationInfoCardState = remember { StationInfoCardState(true) }
+) {
     Card(Modifier.padding(8.dp)) {
         Column(
             Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            Text(text = stringResource(id = R.string.station), style = MaterialTheme.typography.h4)
-            StationInfoElement(title = stringResource(id = R.string.uuid), value = station.uuid)
-            StationInfoElement(
-                title = stringResource(id = R.string.shortname),
-                value = station.shortname
-            )
-            StationInfoElement(
-                title = stringResource(id = R.string.longname),
-                value = station.longname
-            )
-            StationInfoElement(
-                title = stringResource(id = R.string.km),
-                value = stringResource(id = R.string.km_x, station.km.toString())
-            )
-            StationInfoElement(
-                title = stringResource(id = R.string.agency),
-                value = station.agency,
-                isLastItem = true
-            )
-            Text(
-                modifier = Modifier.padding(top = 16.dp),
-                text = stringResource(id = R.string.water),
-                style = MaterialTheme.typography.h4
-            )
-            StationInfoElement(
-                title = stringResource(id = R.string.shortname),
-                value = station.water?.shortname ?: ""
-            )
-            StationInfoElement(
-                title = stringResource(id = R.string.longname),
-                value = station.water?.longname ?: "", isLastItem = true
-            )
+            if (expandState.expanded) {
+                Text(
+                    text = stringResource(id = R.string.station),
+                    style = MaterialTheme.typography.h4
+                )
+                StationInfoElement(title = stringResource(id = R.string.uuid), value = station.uuid)
+                StationInfoElement(
+                    title = stringResource(id = R.string.shortname),
+                    value = station.shortname
+                )
+                StationInfoElement(
+                    title = stringResource(id = R.string.longname),
+                    value = station.longname
+                )
+                StationInfoElement(
+                    title = stringResource(id = R.string.km),
+                    value = stringResource(id = R.string.km_x, station.km.toString())
+                )
+                StationInfoElement(
+                    title = stringResource(id = R.string.agency),
+                    value = station.agency,
+                    isLastItem = true
+                )
+                Text(
+                    modifier = Modifier.padding(top = 16.dp),
+                    text = stringResource(id = R.string.water),
+                    style = MaterialTheme.typography.h4
+                )
+                StationInfoElement(
+                    title = stringResource(id = R.string.shortname),
+                    value = station.water?.shortname ?: ""
+                )
+                StationInfoElement(
+                    title = stringResource(id = R.string.longname),
+                    value = station.water?.longname ?: "", isLastItem = true
+                )
+            }
+            IconButton(
+                onClick = { expandState.expanded = !expandState.expanded },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    if (expandState.expanded) Icons.Default.ExpandLess else Icons.Default.ExpandMore,
+                    contentDescription = if (expandState.expanded) stringResource(id = R.string.collapse) else stringResource(
+                        id = R.string.expand
+                    )
+                )
+            }
         }
     }
 }
